4e476432a870c1423a399914b175c3dc208af341
mason <2224742726@qq.com>
2024-09-17 10:08:31 +0800

    first commit

---
+++ electron.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'), // 可选的预加载脚本
+    },
+  });
+
+  win.loadURL('http://localhost:3000'); // React 应用的地址
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
